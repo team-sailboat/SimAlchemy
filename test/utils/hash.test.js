5e852f3d7ed2ecba4c589eabce018ea23fdaa5fa
Kate Dameron <katerj33@gmail.com>
2019-02-04 11:47:40 -0800

    green test for hash util

---
+++ test/utils/hash.test.js
@@ -0,0 +1,81 @@
+const bcrypt = require('bcryptjs');
+const { hash, compare } = require('../../lib/utils/hash');
+
+describe('bcrypt', () => {
+
+  it('hashes a password', () => {
+    return bcrypt.hash('password', 10)
+      .then(hashedPassword => {
+        expect(hashedPassword).toBeDefined();
+      });
+  });
+
+  it('creates hashed passwords that are different', () => {
+    return bcrypt.hash('password', 10)
+      .then(password1 => {
+        return Promise.all([
+          Promise.resolve(password1),
+          bcrypt.hash('password', 10)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).not.toEqual(hash2);
+      });
+  });
+
+  it('creates the same hash given the same salt', () => {
+    const versionInfo = '$2b$10$';
+    const salt = '12jne9skdn3jdkskwopa98';
+    const bcryptSalt = `${versionInfo}${salt}`;
+    return bcrypt.hash('password', bcryptSalt)
+      .then(password1 => {
+        return Promise.all([
+          Promise.resolve(password1),
+          bcrypt.hash('password', bcryptSalt)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).toEqual(hash2);
+      });
+  });
+
+  it('can compare hashes', () => {
+    return bcrypt.hash('password', 10)
+      .then(hashedPassword => {
+        bcrypt.compare('password', hashedPassword)
+          .then(res => {
+            expect(res).toBeTruthy();
+          });
+      });
+  });
+
+  it('can hash a password via hash function', () => {
+    return hash('password')
+      .then(hashedPass => {
+        expect(hashedPass).toBeDefined();
+        expect(hashedPass).not.toEqual('password');
+      });
+  });
+
+  it('can compare a password and a hash function', () => {
+    const password = 'password';
+    return hash(password)
+      .then(hashedPassword => {
+        return compare(password, hashedPassword);
+      })
+      .then(res => {
+        expect(res).toBeTruthy();
+      });
+  });
+  
+  it('can compare a badPassword and a hash function', () => {
+    const password = 'password';
+    return hash(password)
+      .then(hashedPassword => {
+        return compare('badPassword', hashedPassword);
+      })
+      .then(res => {
+        expect(res).toBeFalsy();
+      });
+  });
+});
