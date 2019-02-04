d27be65b00a6511b5164b989e7c87f3ea5c8d9b6
Paige Gorry <paigeegorry@gmail.com>
2019-02-04 12:05:38 -0800

    can ensure a token is authorized

--- test/middleware/ensureTeacher.test.js
+++ test/middleware/ensureTeacher.test.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-// const { tokenize } = require('../../lib/utils/token');
-const { bearerToken } = require('../../lib/middleware/ensureTeacher');
+const { tokenize } = require('../../lib/utils/token');
+const { bearerToken, ensureAuth } = require('../../lib/middleware/ensureTeacher');
 
 describe('ensureTeacher', () => {
   it('can get a bearer token', () => {
@@ -11,5 +11,16 @@ describe('ensureTeacher', () => {
     bearerToken(req, {}, next);
     expect(req.token).toEqual('abcd123');
     expect(next).toHaveBeenCalled();
+  });
+  it('can ensure a token is authorized', () => {
+    const token = tokenize({ username: 'cari420' });
+    const req = { token };
+    const next = jest.fn();
+
+    ensureAuth(req, {}, next)
+      .then(() => {
+        expect(req.teacher).toEqual({ username: 'cari420' });
+        expect(next).toHaveBeenCalled();
+      });
   });
 });
