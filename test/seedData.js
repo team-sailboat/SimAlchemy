a0847f2387fa940721e5dc17f55496ee946a9459
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-05 09:37:46 -0800

    update seedData to include Assignment model

--- test/seedData.js
+++ test/seedData.js
@@ -1,4 +1,5 @@
 const Teacher = require('../lib/models/Teacher');
+const Assignment = require('../lib/models/Assignment');
 const Cohort = require('../lib/models/Cohort');
 const Chance = require('chance');
 
@@ -6,8 +7,13 @@ const chance = new Chance();
 
 const TOTAL_TEACHERS = 5;
 const TOTAL_COHORTS = 15;
+const TOTAL_ASSIGNMENTS = 30;
 
-module.exports = ({ totalTeachers = TOTAL_TEACHERS, totalCohorts = TOTAL_COHORTS }) => {
+module.exports = ({
+  totalTeachers = TOTAL_TEACHERS,
+  totalCohorts = TOTAL_COHORTS,
+  totalAssignments = TOTAL_ASSIGNMENTS
+}) => {
   return Promise.all([...Array(totalTeachers)].map((el, idx) => {
     return Teacher.create({
       username: `teacher${idx}`,
@@ -18,11 +24,20 @@ module.exports = ({ totalTeachers = TOTAL_TEACHERS, totalCohorts = TOTAL_COHORTS
       return Promise.all([...Array(totalCohorts)].map(() => {
         return Cohort.create({
           teacher: chance.pickone(teachers)._id,
-          stress: 0,
-          sleep: 100,
-          knowledge: 25
+          stress: chance.integer({ min: 0, max: 100 }),
+          sleep: chance.integer({ min: 0, max: 100 }),
+          knowledge: chance.integer({ min: 0, max: 100 })
         });
-      }));
+      }))
+        .then(cohorts => {
+          return Promise.all([...Array(totalAssignments)].map(() => {
+            return Assignment.create({
+              cohortId: chance.pickone(cohorts)._id,
+              name: chance.pickone(['Lab', 'Reading', 'Lecture']),
+              difficulty: chance.pickone(['Easy', 'Medium', 'Hard'])
+            });
+          }));
+        });
     });
 };
 
