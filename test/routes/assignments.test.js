177530f5354b400b9bc6b50df53edbe796a2f5cc
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-05 10:48:40 -0800

    failing post assignments test

---
+++ test/routes/assignments.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../../lib/app');
+const { getToken, getCohort } = require('../dataHelpers');
+
+describe('assignments', () => {
+  it('can post an assignment', () => {
+    return getCohort()
+      .then(cohort => {
+        return request(app)
+          .post('/assignments')
+          .set('Authorization', `Bearer ${getToken()}`)
+          .send({
+            cohortId: cohort._id,
+            name: 'lab',
+            difficulty: 'hard',
+            travis: 'true'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          cohortId: expect.any(String),
+          name: 'lab',
+          difficulty: 'hard',
+          travis: 'true',
+          __v: 0
+        });
+      });
+  });
+});
