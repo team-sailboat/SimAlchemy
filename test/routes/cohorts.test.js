3f9f781a2f28e97b89741629b1f7ade64b9e8c3f
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-05 10:46:16 -0800

    red test for GET /cohorts

--- test/routes/cohorts.test.js
+++ test/routes/cohorts.test.js
@@ -1,31 +1,50 @@
 const request = require('supertest');
 const app = require('../../lib/app');
-const { getTeacher, getToken } = require('../dataHelpers');
+const {
+  getTeacher,
+  getToken,
+  getCohorts
+} = require('../dataHelpers');
 
 describe('cohorts', () => {
   it('can post a cohort', () => {
+    let stress, sleep, knowledge; // use defaults
     return getTeacher()
       .then(teacher => {
         return request(app)
           .post('/cohorts')
-          .set('Authorization', `Bearer ${getToken()}`)
           .send({
             teacher: teacher._id,
-            stress: 100,
-            sleep: 0,
-            knowledge: 6
+            stress,
+            sleep,
+            knowledge
           })
+          .set('Authorization', `Bearer ${getToken()}`)
           .then(res => {
-            console.log(res.body);
             expect(res.body).toEqual({
               teacher: expect.any(String),
-              stress: 100,
-              sleep: 0,
-              knowledge: 6,
+              stress: 0,
+              sleep: 100,
+              knowledge: 25,
               _id: expect.any(String),
               __v: 0
             });
           });
+      });
+  });
+
+  it('can get all cohorts', () => {
+    return request(app)
+      .get('/cohorts')
+      .set('Authorization', `Bearer ${getToken()}`)
+      .then(res => {
+        return Promise.all([
+          Promise.resolve(res.body),
+          getCohorts()
+        ]);
+      })
+      .then(([body, cohorts]) => {
+        expect(body).toHaveLength(cohorts.length);
       });
   });
 });
