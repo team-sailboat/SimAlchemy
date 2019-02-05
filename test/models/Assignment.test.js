1533ff25b43c5bc61c7bc73a90b8dd893f499d60
cari palazzolo <42423180+caripizza@users.noreply.github.com>
2019-02-04 17:47:24 -0800

    merge dev > master (#5)
    * adding dev branch
    
    * initial setup
    
    * Remove the now ignored directory node_modules
    
    * initial scaffolding and set-up, brought in middleware for error and notfound, server running!
    
    * tweak REAMDE links and update package.json authors
    
    * added .eslintrc and initial app test
    
    * can validate a good model
    
    * username required
    
    * can store a temp password and hash a password
    
    * green tests for compare good password and bad password
    
    * green test for Teacher model password token and auth token
    
    * green test for hash util
    
    * green test for token util
    
    * can get a bearer token
    
    * can ensure a token is authorized
    
    * wrote test for auth/signup
    
    * signup route passing test!
    
    * can signin a teacher test written
    
    * can signin a teacher test passing!
    
    * red test for auth /verify route
    
    * green test for auth /verify route
    
    * added test for auth/signin to check if bad password
    
    * test for auth/signin to check if wrong username
    
    * started seedData, propogates 5 teachers to db
    
    * added dataHelpers
    
    * added getters
    
    * took out random number from seedData
    
    * added client, script, tokens, and can SIGNIN
    
    * added mask to our password
    
    * added signing validator
    
    * inquirer initial selection / signup / signin prompts
    
    * found error message for bad signin!"
    
    * reprompt added after bad signin error
    
    * add figlet as dependency and include console log welcome msg for client
    
    * update travis to use correct db
    
    * Added the cohort model and test; merging so we can use it in assignments model!  (#3)
    
    * validating a good Cohort model
    
    * removed connection so npm run test will close connection
    
    * Assignment Model and validation test! (#4)
    
    * Assignment model - green test passing validation of a good model
    
    * assignment route/test files started
    
    * delete empty test file
    
    * updated cohort seed data and data helper + validation test for assignment model
    
    * fix extra parens from botched merge conflict

---
+++ test/models/Assignment.test.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Assignment = require('../../lib/models/Assignment');
+const { getTeacher, getCohort } = require('../dataHelpers');
+
+describe('Assignment Model', () => {
+
+  it('validates a good model', async() => {
+    const teacher = await getTeacher();
+    const cohort = await getCohort({ teacher: teacher._id });
+    const assignment = new Assignment({
+      cohortId: cohort._id,
+      name: 'lab',
+      difficulty: 'hard'
+    });
+    expect(assignment.toJSON()).toEqual({
+      _id: expect.any(Object),
+      cohortId: expect.any(mongoose.Types.ObjectId),
+      name: 'lab',
+      difficulty: 'hard'
+    });
+  });
+
+});
+
