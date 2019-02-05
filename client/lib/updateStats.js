fcd7d27a879bd68ad7a77c607f15e9bdb01567d0
Kate Dameron <katerj33@gmail.com>
2019-02-05 15:57:22 -0800

    prompt to show updated stats for cohort

--- client/lib/updateStats.js
+++ client/lib/updateStats.js
@@ -2,7 +2,6 @@ const config = require('../config');
 const request = require('superagent');
 const inquirer = require('inquirer');
 const { getToken } = require('../helper/tokens');
-const Cohort = require('../../lib/models/Cohort');
 
 const updateStats = (id, difficulty) => {
   return request
@@ -34,10 +33,29 @@ const updateStats = (id, difficulty) => {
           knowledge: newKnowledge,
           stress: newStress
         });
+    })
+    .then(() => {
+      return request
+        .get(`${config.url}/cohorts/${id}`)
+        .set('Authorization', `Bearer ${getToken()}`)
+        .then(res => {
+          const { stress, sleep, knowledge } = res.body;
+          return inquirer.prompt([
+            {
+              type: 'list',
+              name: 'continue',
+              message: `Here are your updated Cohort stats:
+              stress: ${stress},
+              sleep: ${sleep},
+              knowledge: ${knowledge}`,
+              choices: [{
+                name: 'continue',
+                value: 'continue'
+              }]
+            }
+          ]);
+        });
     });
-
-    
-
   // return inquirer.prompt([
   //   {
   //     type:'list',
