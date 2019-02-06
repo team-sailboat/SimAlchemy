c3ced12cd183ec482f4a035a96a115e541a64fe2
Kate Dameron <katerj33@gmail.com>
2019-02-06 12:09:36 -0800

    new checkAssignLength function checking for 5 total assignments to send to win case prompt

--- client/lib/updateStats.js
+++ client/lib/updateStats.js
@@ -2,27 +2,37 @@ const config = require('../config');
 const request = require('superagent');
 const inquirer = require('inquirer');
 const { getToken } = require('../helper/tokens');
+const checkAssignLength = require('../lib/checkAssignLength');
 
 const updateStats = (id) => {
-  return request
-    .get(`${config.url}/cohorts/${id}`)
-    .set('Authorization', `Bearer ${getToken()}`)
-    .then(res => {
-      const { stress, sleep, knowledge } = res.body;
-      return inquirer.prompt([
-        {
-          type: 'list',
-          name: 'continue',
-          message: `Here are your updated Cohort stats:
-              stress: ${stress},
-              sleep: ${sleep},
-              knowledge: ${knowledge}`,
-          choices: [{
-            name: 'continue',
-            value: 'continue'
-          }]
-        }
-      ]);
+  return checkAssignLength(id)
+    .then(length => {
+      if(length < 5) {
+        return request
+          .get(`${config.url}/cohorts/${id}`)
+          .set('Authorization', `Bearer ${getToken()}`)
+          .then(res => {
+            const { stress, sleep, knowledge } = res.body;
+            return inquirer.prompt([
+              {
+                type: 'list',
+                name: 'continue',
+                message: `Here are your updated Cohort stats:
+                  stress: ${stress},
+                  sleep: ${sleep},
+                  knowledge: ${knowledge}`,
+                choices: [{
+                  name: 'continue',
+                  value: 'continue'
+                }]
+              }
+            ]);
+          });
+      }
+      else {
+        console.log('game over');
+      }
     });
+
 };
 module.exports = updateStats;
