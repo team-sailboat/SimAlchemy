fe2a9c080df180be12be0d16098bbf790c28c63d
Kate Dameron <katerj33@gmail.com>
2019-02-06 17:57:02 -0800

    more conditionals + messages for assignmentPost

--- client/lib/genMsg.js
+++ client/lib/genMsg.js
@@ -11,23 +11,25 @@ const genMsg = id => {
       if(stress === 25 && sleep === 100) {
         return 'It\'s time to post your first assignment. Choose an assignment:';
       }
-      else if((stress > 25 && stress <= 35) && (sleep >= 90 && sleep < 100)) {
+      else if((stress > 25 && stress <= 30) && (sleep >= 90 && sleep < 100)) {
         return 'Good Job! Your cohort isn\'t too stressed yet. Time for another assignment!';
       }
-      else if((knowledge >= 50 && knowledge <= 60) && (sleep <= 50 && sleep >= 40)) {
+      else if((knowledge >= 25 && knowledge <= 50) && (sleep >= 55 && sleep <= 65)) {
+        return 'Your cohort is losing sleep fast and they are having trouble retaining knowledge. It might be time for a lecture to review!';
+      }
+      else if((knowledge >= 50 && knowledge <= 60) && (sleep <= 55 && sleep >= 40)) {
         return 'Your cohort is learning SO MUCH but they\'re exhausted! How about something a little easier?';
       }
       else if(stress >= 70) {
         return 'Uh Oh, peeps be stressssed! Better think about this next assignment carefully.';
       }
-      else if(knowledge >= 75 && sleep >= 50 && stress <= 50) {
+      else if(knowledge >= 75 && sleep >= 50 && stress <= 65) {
         return 'You\'re doing great! Your cohort is on fire and their stress and sleep levels are healthy.';
       }
       else if((stress >= 85) && (sleep <= 20)) {
         return 'This isn\'t looking good. Your cohort has been working nonstop. At this rate they\'re approaching burnout quickly. AARRGGHH!';
       }
       else return 'Try another assignment!';
-      // else if(blahblah) {}
     });
 };
 
