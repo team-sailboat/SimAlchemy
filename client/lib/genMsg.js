6bf2ffa55a2c0e0280d08c69486db57fcb4b1b18
Paige Gorry <paigeegorry@gmail.com>
2019-02-07 09:31:39 -0800

    added chalk colors to genMsg

--- client/lib/genMsg.js
+++ client/lib/genMsg.js
@@ -2,6 +2,7 @@ const config = require('../config');
 const request = require('superagent');
 const { getToken } = require('../helper/tokens');
 const figlet = require('figlet');
+const chalk = require('chalk');
 
 const genMsg = id => {
   return request
@@ -15,25 +16,25 @@ const genMsg = id => {
           horizontalLayout: 'default',
           verticalLayout: 'default'
         }));
-        return 'It\'s time to post your first assignment. Choose an assignment:';
+        return chalk.green('It\'s time to post your first assignment. Choose an assignment:');
       }
       else if((stress > 25 && stress <= 30) && (sleep >= 90 && sleep < 100)) {
-        return 'Good Job! Your cohort isn\'t too stressed yet. Time for another assignment!';
+        return chalk.green('Good Job! Your cohort isn\'t too stressed yet. Time for another assignment!');
       }
       else if((knowledge >= 25 && knowledge <= 50) && (sleep >= 55 && sleep <= 65)) {
-        return 'Your cohort is losing sleep fast and they are having trouble retaining knowledge. It might be time for a lecture to review!';
+        return chalk.yellow('Your cohort is losing sleep fast and they are having trouble retaining knowledge. It might be time for a lecture to review!');
       }
       else if((knowledge >= 50 && knowledge <= 60) && (sleep <= 55 && sleep >= 40)) {
-        return 'Your cohort is learning SO MUCH but they\'re exhausted! How about something a little easier?';
+        return chalk.yellow('Your cohort is learning SO MUCH but they\'re exhausted! How about something a little easier?');
       }
       else if(stress >= 70) {
-        return 'Uh Oh, peeps be stressssed! Better think about this next assignment carefully.';
+        return chalk.red('Uh Oh, peeps be stressssed! Better think about this next assignment carefully.');
       }
       else if(knowledge >= 75 && sleep >= 50 && stress <= 65) {
-        return 'You\'re doing great! Your cohort is on fire and their stress and sleep levels are healthy.';
+        return chalk.green('You\'re doing great! Your cohort is on fire and their stress and sleep levels are healthy.');
       }
       else if((stress >= 85) && (sleep <= 20)) {
-        return 'This isn\'t looking good. Your cohort has been working nonstop. At this rate they\'re approaching burnout quickly. AARRGGHH!';
+        return chalk.red('This isn\'t looking good. Your cohort has been working nonstop. At this rate they\'re approaching burnout quickly. AARRGGHH!');
       }
       else return 'Try another assignment!';
     });
