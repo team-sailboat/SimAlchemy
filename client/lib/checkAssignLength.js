47be8af5898fc0dc98232d0c44f109aba572c20a
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-07 17:49:18 -0800

    added console logs for in between game prompts

--- client/lib/checkAssignLength.js
+++ client/lib/checkAssignLength.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const config = require('../config');
 const request = require('superagent');
 const { getToken } = require('../helper/tokens');
@@ -6,7 +7,17 @@ const checkAssignLength = (id) => {
   return request
     .get(`${config.url}/cohorts/${id}`)
     .set('Authorization', `Bearer ${getToken()}`)
-    .then(cohort => cohort.body.assignments.length);
+    .then(cohort => {
+      const tries = 5 - cohort.body.assignments.length;
+      if(tries === 1) {
+        console.log(tries + ' try left');
+      } else if(tries > 1 && tries < 5) {
+        console.log(tries + ' tries left');
+      } else if(tries === 0) {
+        console.log('No more tries left');
+      }
+      return cohort.body.assignments.length;
+    });
 };
 
 module.exports = checkAssignLength;
