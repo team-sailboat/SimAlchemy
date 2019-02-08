d0a3ba3a3533c5746e97e733237d06d39cfb8e9c
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-07 18:58:52 -0800

    fix duplicated code in client token.js

--- client/helper/tokens.js
+++ client/helper/tokens.js
@@ -6,10 +6,6 @@ const setToken = t => token = t;
 const getTeach = () => teacher;
 const setTeach = st => teacher = st;
 
-const getTeach = () => teacher;
-
-const setTeach = st => teacher = st;
-
 module.exports = {
   getToken,
   setToken,
